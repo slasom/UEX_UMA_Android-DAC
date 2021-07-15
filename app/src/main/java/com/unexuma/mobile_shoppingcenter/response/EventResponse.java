405e48df603141f9390afafa1c64d897322636b0
slasom <slasom@unex.es>
2021-07-15 10:26:12 +0200

    Update app

---
+++ app/src/main/java/com/unexuma/mobile_shoppingcenter/response/EventResponse.java
@@ -0,0 +1,83 @@
+package com.unexuma.mobile_shoppingcenter.response;
+
+
+
+import com.google.gson.annotations.SerializedName;
+import com.unexuma.mobile_shoppingcenter.model.Event;
+
+
+public class EventResponse {
+
+    @SerializedName("resource")
+    private String resource;
+
+    @SerializedName("method")
+    private String method;
+
+    @SerializedName("params")
+    private Params params;
+
+    @SerializedName("sender")
+    private String sender;
+
+    @SerializedName("idRequest")
+    private String idRequest;
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public Params getParams() {
+        return params;
+    }
+
+    public void setParams(Params params) {
+        this.params = params;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getIdRequest() {
+        return idRequest;
+    }
+
+    public void setIdRequest(String idRequest) {
+        this.idRequest = idRequest;
+    }
+
+    public class Params {
+    
+      
+        @SerializedName("event")
+        private Event event;
+      
+        public Event getevent() {
+            return event;
+        }
+
+
+    }
+
+
+}
+
+
+
---
+++ app/src/main/java/com/unexuma/mobile_shoppingcenter/service/MQTTConfiguration.java
@@ -0,0 +1,7 @@
+package com.unexuma.mobile_shoppingcenter.service;
+
+public class MQTTConfiguration {
+
+    public static String MQTT_BROKER_URL = "tcp://broker.hivemq.com:1883";
+
+}
