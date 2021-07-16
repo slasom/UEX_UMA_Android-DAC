a0496140aa629e8843062cf52853c3fafe0eaae0
slasom <slasom@unex.es>
2021-07-16 12:10:32 +0200

    Evaluation

--- app/src/main/java/com/unexuma/mobile_shoppingcenter/service/MQTTConfiguration.java
+++ app/src/main/java/com/unexuma/mobile_shoppingcenter/service/MQTTConfiguration.java
@@ -2,6 +2,6 @@ package com.unexuma.mobile_shoppingcenter.service;
 
 public class MQTTConfiguration {
 
-    public static String MQTT_BROKER_URL = "tcp://broker.hivemq.com:1883";
+    public static String MQTT_BROKER_URL = "tcp://108.129.48.139:1883";
 
 }
